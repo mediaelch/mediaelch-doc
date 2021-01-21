5d561a133bc3fbe45ebae1bf33440859333cdb14
Andre Meyering <info@andremeyering.de>
2021-01-21 18:57:38 +0100

    [Release] Prepare release of v2.8.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = ''
 # The short X.Y version
 version = u'2.8'
 # The full version, including alpha/beta/rc tags
-release = u'2.8.4'
+release = u'2.8.6'
 
 
 # -- General configuration ---------------------------------------------------
