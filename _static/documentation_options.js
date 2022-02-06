216aad8c16956093e9a87a050e5c81bf41410eaa
Andre Meyering <info@andremeyering.de>
2022-02-06 16:59:15 +0100

    new pages version So 6. Feb 16:59:15 CET 2022

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,10 +3,8 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '2.8.14',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
 };
